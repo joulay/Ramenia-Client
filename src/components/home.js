15aa48dccbef7a7fc8c603b475aaed540cac4349
Julie K <julieskim160@gmail.com>
2018-09-13 19:51:43 -0700

    changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,17 @@
 import React from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 
 export default function Home() {
     return (
-        
+        <Router>
             <div className="home">
                 <main>
-
+                    <Switch>
+                        <Route exact path="/signup" component={SignUp} />
+                    </Switch>
                 </main>
             </div>
-        
+        </Router>
     )
 }
 
